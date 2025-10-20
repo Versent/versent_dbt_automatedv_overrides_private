504fbd0cb0f253b6d376cb04d0317fe5cbb14f9b
merisaindajang-versent <merisa.indajang@versent.com.au>
2025-10-20 16:14:05 +1100

    fix the hk_ghost_default macro

--- macros/automate_dv_overrides/hk_ghost.sql
+++ macros/automate_dv_overrides/hk_ghost.sql
@@ -4,7 +4,3 @@ coalesce (
     {{ hk_ghost_default()}}
     ) as {{ column_name }}
 {%- endmacro %}
-
-{% macro hk_ghost_default()%}
-    {{ automate_dv.hash("'ghost'", 'hk_ghost')}}
-{%- endmacro %}
---
+++ macros/automate_dv_overrides/hk_ghost_default.sql
@@ -0,0 +1,3 @@
+{% macro hk_ghost_default()%}
+    {{ automate_dv.hash("'ghost'", 'hk_ghost')}}
+{%- endmacro %}
