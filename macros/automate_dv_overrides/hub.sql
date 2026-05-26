48da43b84837df4dbc63a01aa4f8e48e4cfa8aa3
TimNapier <tim.napier@hotmail.com>
2026-05-27 09:30:08 +1000

    Changed hub macro to use automatedv.hash macro

--- macros/automate_dv_overrides/hub.sql
+++ macros/automate_dv_overrides/hub.sql
@@ -1,5 +1,5 @@
 {%- macro hub(src_pk, src_nk, src_extra_columns, src_ldts, src_source, source_model) -%}
-
+{% set var_bk_ghost = var('bk_ghost') %}
 {{- automate_dv.default__hub(src_pk=src_pk,
                              src_nk=src_nk,
                              src_extra_columns=src_extra_columns,
@@ -10,10 +10,13 @@
 
 {# Overrides automate_dv.hub macro to add 'ghost' record #}
 UNION 
-SELECT  cast('{{ var('bk_ghost') }}' as binary) AS {{src_pk}},
-        '{{ var('bk_ghost') }}' as {{src_nk}},
+SELECT  {{ automate_dv.hash(src_nk, src_pk) }},
+        {{ src_nk}},
         current_timestamp() as load_datetime,
         'macros_hub' as record_source
-                    
+    from
+        (
+            select 
+            '{{ var_bk_ghost }}' as {{src_nk}})
  
 {%- endmacro -%}
